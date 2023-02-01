40c712ded184f15a552657df8721ebab5fa9d7ae
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-02-01 16:48:55 +0530

    different ways of writing a javascript

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// ways to print
+console.log("I love you");
+alert("I love you");
+document.write("hello world");
+
+// console api
+console.error("This is an error");
+console.warn("This is warning");
+console.assert(4==6);
