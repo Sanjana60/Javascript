2a50d813f195e01c13e8fc06d9eaff8fbc4d9885
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-02-01 17:05:45 +0530

    data types in javascript

--- js/index.js
+++ js/index.js
@@ -12,3 +12,34 @@ console.assert(4==6);
 var number1 = 2;
 var number2 = 2;
 console.log(number1+number2);
+
+var str1 = "aryan";
+var str2 = 'sanjana';
+
+console.log(str1);
+console.log(str2);
+
+
+var marks = {
+
+    aryan: 56,
+    sanjan:56,
+
+}
+
+console.log(marks);
+
+var a = true;
+var b = false;
+console.log(a,b);
+
+var sun;
+console.log(sun);
+
+var ary = null;
+console.log(ary);
+
+
+var arr = [0,1,2,3,4,5,'sanj','aryan'];
+console.log(arr);
+console.log(arr[5]);
